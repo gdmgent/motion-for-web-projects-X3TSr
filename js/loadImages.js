a5aab0b6afcc91624a4ddd4749f7edd6a79bb889
X3TSr <62379105+X3TSr@users.noreply.github.com>
2023-03-16 12:04:16 +0100

    Added image preloading and more effect refining

--- js/loadImages.js
+++ js/loadImages.js
@@ -1,13 +1,7 @@
-const preload = document.getElementById("preload");
 const cardImgs = document.getElementsByClassName("cardImg");
 const arrCardImgs = [...cardImgs];
-let i = 0
-let j = 0;
 
 arrCardImgs.forEach(cardImg => {
-    let img = document.createElement("img");
-    img.src = "https://source.unsplash.com/featured/1080x19"+j+i;
-    preload.appendChild(img);
     cardImg.src = "https://source.unsplash.com/featured/1080x19"+j+i;
     i==9? j++ : i++;
     i==9? i=0 : 0;
