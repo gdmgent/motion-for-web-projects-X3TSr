08a1af5a9fc0232a662f962bef182666bea71daa
X3TSr <62379105+X3TSr@users.noreply.github.com>
2023-03-20 12:52:51 +0100

    Added shine to the cards when hovering + 3d effect

--- js/loadImages.js
+++ js/loadImages.js
@@ -2,7 +2,7 @@ const cardImgs = document.getElementsByClassName("cardImg");
 const arrCardImgs = [...cardImgs];
 
 arrCardImgs.forEach(cardImg => {
-    cardImg.src = "https://source.unsplash.com/featured/1080x19"+j+i;
+    cardImg.src = "https://unsplash.it/1080/19"+j+i;
     i==9? j++ : i++;
     i==9? i=0 : 0;
 });
--- js/preloadImages.js
+++ js/preloadImages.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 3; i++) {
     const link = document.createElement('link');
     link.rel = 'preload';
     link.as = 'image';
-    link.href = "https://source.unsplash.com/featured/1080x19"+j+i;
+    link.href = "https://unsplash.it/1080/19"+j+i;
     head.appendChild(link);
     i==9? j++ : i++;
     i==9? i=0 : 0;
