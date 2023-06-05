5dfedc3a726c6e3c8bfbeaeef0dc69177e2cc675
X3TSr <62379105+X3TSr@users.noreply.github.com>
2023-06-05 15:37:33 +0200

    Added "#kyllen" to not break anyone elses project

--- Exercises/05 - Ceci n'est pas un carré/Eindopdracht/js/kyllen.js
+++ Exercises/05 - Ceci n'est pas un carré/Eindopdracht/js/kyllen.js
@@ -6,8 +6,8 @@ function kyllen() {
         section.tl
         
 
-        .add("makeEgg")
-        .to(".box", {
+        .add("#kyllen makeEgg")
+        .to("#kyllen .box", {
             borderRadius: "80% 40% 80% 40%",
             boxShadow: `
             rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, 
@@ -20,111 +20,111 @@ function kyllen() {
             rgba(0, 0, 0, 0.09) 0px 32px 16px
             `,
             rotate: "260deg"
-        }, "makeEgg")
+        }, "#kyllen makeEgg")
 
 
         
-        .add("crack")
-        .to(".part1", {
+        .add("#kyllen crack")
+        .to("#kyllen .part1", {
             width: "19px",
             height: "33px",
             visibility: "visible"
-        }, "crack")
-        .to(".part2", {
+        }, "#kyllen crack")
+        .to("#kyllen .part2", {
             width: "19px",
             height: "33px",
             visibility: "visible"
-        }, "crack")
+        }, "#kyllen crack")
 
 
 
         
-        .add("pan")
-        .to(".fryingPan", {
+        .add("#kyllen pan")
+        .to("#kyllen .fryingPan", {
             opacity: "1",
             transform: "translate(-8%, -58%) perspective(1000px) rotateX(50deg)"
-        }, "pan")
-        .to(".box", {
+        }, "#kyllen pan")
+        .to("#kyllen .box", {
             borderRadius: "60% 60% 60% 60%",
             rotate: "0deg",
             width: "14rem",
             height: "14rem",
             transform: "translate(-50%, -50%) perspective(1000px) rotateX(60deg)"
-        }, "pan")
-        .to(".part1", {
+        }, "#kyllen pan")
+        .to("#kyllen .part1", {
             width: "0",
             height: "0",
             opacity: "0"
-        }, "pan")
-        .to(".part2", {
+        }, "#kyllen pan")
+        .to("#kyllen .part2", {
             width: "0",
             height: "0",
             opacity: "0"
-        }, "pan")
-        .to(".yoke", {
+        }, "#kyllen pan")
+        .to("#kyllen .yoke", {
             width: "7rem",
             height: "7rem"
-        }, "pan")
+        }, "#kyllen pan")
 
 
 
-        .add("yoke")
-        .to(".yoke", {
+        .add("#kyllen yoke")
+        .to("#kyllen .yoke", {
             opacity: "1",
             width: "7rem",
             height: "7rem"
-        }, "yoke")
-        .to(".box", {
+        }, "#kyllen yoke")
+        .to("#kyllen .box", {
             transform: "translate(-50%, -30%) perspective(1000px) rotateX(0deg)"
-        }, "yoke")
-        .to(".fryingPan", {
+        }, "#kyllen yoke")
+        .to("#kyllen .fryingPan", {
             transform: "translate(-8%, -58%) perspective(1000px) rotateX(0deg)"
-        }, "yoke")
+        }, "#kyllen yoke")
 
 
 
 
-        .add("wink", ">.3")
-        .to(".yoke", {
+        .add("#kyllen wink", ">.3")
+        .to("#kyllen .yoke", {
             height: "0"
-        }, "wink")
+        }, "#kyllen wink")
         .to(".yoke", {
             backgroundColor: "lightblue"
-        }, "wink")
+        }, "#kyllen wink")
 
-        .add("wink2")
-        .to(".yoke", {
+        .add("#kyllen wink2")
+        .to("#kyllen .yoke", {
             height: "7rem",
             boxShadow: "inset 0 0 1rem 0.1rem black"
-        }, "wink2")
-        .to(".pupil", {
+        }, "#kyllen wink2")
+        .to("#kyllen .pupil", {
             height: "3.5rem",
             boxShadow: "0 0 1rem 0.1rem white"
-        }, "wink2")
-        .to(".eye2", {
+        }, "#kyllen wink2")
+        .to("#kyllen .eye2", {
             visibility: "visible"
-        }, "wink2")
+        }, "#kyllen wink2")
         
         
 
-        .add("void")
-        .to(".nothing", {x:0}, "void")
-        .to(".eureka", {
+        .add("#kyllen void")
+        .to("#kyllen .nothing", {x:0}, "#kyllen void")
+        .to("#kyllen .eureka", {
             y: 1000
-        }, "wink2")
+        }, "#kyllen wink2")
 
         
-        .add("end")
-        .to(".yoke", {
+        .add("#kyllen end")
+        .to("#kyllen .yoke", {
             opacity: "0"
-        }, "end")
-        .to(".pupil", {
+        }, "#kyllen end")
+        .to("#kyllen .pupil", {
             opacity: "0"
-        }, "end")
-        .to(".eye2", {
+        }, "#kyllen end")
+        .to("#kyllen .eye2", {
             opacity: "0"
-        }, "end")
-        .to(".box", {
+        }, "#kyllen end")
+        .to("#kyllen .box", {
             rotate: "0",
             boxShadow: `
             rgba(0, 0, 0, 0) 0px 0px 0px 0px inset, 
@@ -140,6 +140,6 @@ function kyllen() {
             transform: "",
             height: "20vw",
             width: "20vw"
-        }, "end")
+        }, "#kyllen end")
     }
 }
